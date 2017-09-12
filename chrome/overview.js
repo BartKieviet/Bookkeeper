BartKieviet/Bookkeeper

commit b8e3c16056514e3e80bc3910adfd6da45fb3a374

Renaming to Pardus Bookkeeper part one
Remains changing the Chrome Store URL probably.

chrome/bookkeeper.css
@@ -1,11 +1,11 @@
 /* -*- css-indent-offset: 8; indent-tabs-mode: t -*- */
 
-h1.copilot img {
+h1.bookkeeper img {
 	vertical-align: -2px;
 	margin-right: 4px;
 }
 
-#copilot-overview {
+#bookkeeper-overview {
 	display: inline-block;
 	width: auto;
 	border: outset 1px #A0B1C9;
@@ -13,12 +13,12 @@ h1.copilot img {
 	padding: 5px;
 }
 
-#copilot-overview table {
+#bookkeeper-overview table {
 	text-align: inherit;
 	border-collapse: collapse;
 }
 
-#copilot-overview th {
+#bookkeeper-overview th {
 	padding: 0 2px;
 	background: #a1a1af;
 	color: #00001c;
@@ -28,38 +28,38 @@ h1.copilot img {
 	border-right: solid 2px #00001c;
 }
 
-#copilot-overview th.c {
+#bookkeeper-overview th.c {
 	background: #3b3b49;
 	border: none;
 }
 
-#copilot-overview th.sort {
+#bookkeeper-overview th.sort {
 	cursor: pointer;
 }
 
-#copilot-overview tbody {
+#bookkeeper-overview tbody {
 	cursor: crosshair;
 }
 
-#copilot-overview tbody tr:hover {
+#bookkeeper-overview tbody tr:hover {
 	background-color: #1a1a36;
 }
 
-#copilot-overview td {
+#bookkeeper-overview td {
 	border: solid 1px #1a1a36;
 }
 
-#copilot-overview td.r,
-#copilot-overview td.c {
+#bookkeeper-overview td.r,
+#bookkeeper-overview td.c {
 	text-align: right;
 }
 
-#copilot-overview td.red{	
+#bookkeeper-overview td.red{
 	color:red;
 	text-align: right;
 }
 
-#copilot-overview td.yellow{	
+#bookkeeper-overview td.yellow{
 	color:yellow;
 	text-align: right;
 }

chrome/overview.js
@@ -270,17 +270,17 @@ function showOverviewBuildings( data ) {
 
 	// Build the table and headers
 	container = document.createElement( 'div' );
-	container.id = 'copilot-overview';
+	container.id = 'bookkeeper-overview';
 
 	table = document.createElement( 'table' );
 	thead = document.createElement( 'thead' );
 
 	tr = document.createElement( 'tr' );
-	addTH( tr, 'Location', 'sort', 'copilot-hdr-loc' );
-	addTH( tr, 'Type', 'sort', 'copilot-hdr-type' );
-	addTH( tr, 'Owner', 'sort', 'copilot-hdr-owner' );
-	addTH( tr, 'Lvl', 'sort', 'copilot-hdr-level' );
-	
+	addTH( tr, 'Location', 'sort', 'bookkeeper-hdr-loc' );
+	addTH( tr, 'Type', 'sort', 'bookkeeper-hdr-type' );
+	addTH( tr, 'Owner', 'sort', 'bookkeeper-hdr-owner' );
+	addTH( tr, 'Lvl', 'sort', 'bookkeeper-hdr-level' );
+
 	for( i = 0, end = in_use.length; i < end; i++ ) {
 		ckey = in_use[i];
 		commodity = COMMODITIES[ckey];
@@ -291,10 +291,10 @@ function showOverviewBuildings( data ) {
 		addTH( tr, img, 'c' );
 	}
 
-	addTH( tr, 'Updated', 'sort', 'copilot-hdr-time' );
-	addTH( tr, 'Ticks', 'sort', 'copilot-hdr-ticks' );
-	addTH( tr, 'Ticks left', 'sort', 'copilot-hdr-ticksLeft' );
-	
+	addTH( tr, 'Updated', 'sort', 'bookkeeper-hdr-time' );
+	addTH( tr, 'Ticks', 'sort', 'bookkeeper-hdr-ticks' );
+	addTH( tr, 'Ticks left', 'sort', 'bookkeeper-hdr-ticksLeft' );
+
 	addTH( tr, '' ); // the bin icon column
 	thead.appendChild( tr );
 	thead.addEventListener( 'click', onHeaderClick, false );
@@ -310,18 +310,19 @@ function showOverviewBuildings( data ) {
 	var anchor = document.getElementsByTagName('h1')[0];
 
 	h1 = document.createElement( 'h1' );
-	h1.className = 'copilot';
+	h1.className = 'bookkeeper';
 	img = document.createElement( 'img' );
 	img.src = chrome.extension.getURL( 'icons/24.png' );
+	img.title = 'Pardus Bookkeeper';
 	h1.appendChild( img );
-	h1.appendChild( document.createTextNode('Copilot Overview') );
+	h1.appendChild( document.createTextNode('Bookkeeping') );
 
 	anchor.parentNode.insertBefore( h1, anchor );
 	anchor.parentNode.insertBefore( container, anchor );
 
 	function onHeaderClick( event ) {
 		var target = event.target;
-		if( target.id && target.id.startsWith( 'copilot-hdr-' ) ) {
+		if( target.id && target.id.startsWith( 'bookkeeper-hdr-' ) ) {
 			event.stopPropagation();
 			var newsort = target.id.substr( 12 );
 			if( newsort == sort )
@@ -390,7 +391,7 @@ function fillTBody( tbody, in_use, buildings, sort, ascending ) {
 		tr.appendChild( cell );
 
 		addTD( tr, building.ticks < Infinity ? String(building.ticks) : '??', 'r' );
-		
+
 		var tickPassedClass = 'r';
 		if (building.ticks < Infinity) {
 			// Not pretty but fast according to:
@@ -398,9 +399,9 @@ function fillTBody( tbody, in_use, buildings, sort, ascending ) {
 			if ((building.ticks - building.ticksPassed ) < 2) { tickPassedClass = "red"; } else
 			if ((building.ticks - building.ticksPassed ) < 3) { tickPassedClass = "yellow"; }
 		}
-		
+
 		addTD( tr, building.ticks < Infinity ? String(building.ticks - building.ticksPassed ) : '??', tickPassedClass );
-		
+
 		img = document.createElement("img");
 		img.src = "http://static.pardus.at/img/stdhq/ui_trash.png";
 		img.onclick = function() {

chrome/building.js
@@ -14,7 +14,7 @@ function configure() {
 		document.defaultView.addEventListener( 'message', onGameMessage );
 		var script = document.createElement( 'script' );
 		script.type = 'text/javascript';
-		script.textContent = "(function() {var fn=function(){window.postMessage({pardus_copilot:1,\
+		script.textContent = "(function() {var fn=function(){window.postMessage({pardus_bookkeeper:1,\
 		loc:typeof(userloc)=='undefined'?null:userloc,\
 		res_upkeep:typeof(res_upkeep)=='undefined'?null:res_upkeep,\
 		res_production:typeof(res_production)=='undefined'?null:res_production,\
@@ -36,7 +36,7 @@ function configure() {
 function onGameMessage( event ) {
 	var data = event.data;
 
-	if ( !data || data.pardus_copilot != 1 ) {
+	if ( !data || data.pardus_bookkeeper != 1 ) {
 		return;
 	}
 

chrome/nav.js
@@ -15,7 +15,7 @@ function configure() {
 	window.addEventListener( 'message', onMessage, false );
 	var script = doc.createElement( 'script' );
 	script.type = 'text/javascript';
-	script.textContent = "(function() {var fn=function(){window.postMessage({copilot:1,loc:typeof(userloc)=='undefined'?null:userloc},window.location.origin);};if(typeof(addUserFunction)=='function') addUserFunction(fn);fn();})();";
+	script.textContent = "(function() {var fn=function(){window.postMessage({bookkeeper:2,loc:typeof(userloc)=='undefined'?null:userloc},window.location.origin);};if(typeof(addUserFunction)=='function') addUserFunction(fn);fn();})();";
 	doc.body.appendChild(script);
 }
 
@@ -23,7 +23,7 @@ function onMessage( event ) {
 	var element, sector, m, x, y,
 	    data = event.data;
 
-	if( data.copilot != 1 )
+	if( data.bookkeeper != 2 )
 		return;
 
 	event.stopPropagation();

chrome/popup.html
@@ -1,10 +1,27 @@
 <html>
-<head>
-</head>
-<div><h1>Pardus copilot</h1></div>
-<div style="min-width: 200px">
-Go to a building and press "track" to get an overview of the tracked buildings in the building overview page.
-</div>
-<div><h2>Credits</h2>
-<div>Credit icon made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+ <head>
+  <title>Pardus Bookkeeper</title>
+  <style>
+   html { font-family: system-ui, sans-serif }
+   div#bookeeper-popup { min-width: 200px }
+  </style>
+ </head>
+ <body>
+  <div id="bookeeper-popup">
+   <h1>Pardus Bookkeeper</h1>
+
+   <p>Go to a building and press "track" to get an overview of the tracked buildings in the building overview page.</p>
+
+   <h2>Credits</h2>
+   <p>
+    Icon by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from
+    <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>,
+    licensed under
+
+    <a href="http://creativecommons.org/licenses/by/3.0/"
+       title="Creative Commons BY 3.0"
+       target="_blank">CC 3.0 BY</a>
+   </p>
+  </div>
+ </body>
 </html>

commit 819c9474c9b285b09c561fa05bd92e5f6c6d029e

Sorting works again, by ticks left even
 * Fixing breakage caused by b8e3c16056514e3e80bc3910adfd6da45fb3a374
 * Adding sorting by ticks left

chrome/bookkeeper.css
@@ -54,12 +54,12 @@ h1.bookkeeper img {
 	text-align: right;
 }
 
-#bookkeeper-overview td.red{
-	color:red;
-	text-align: right;
+#bookkeeper-overview td.red {
+	color: red;
+	font-weight: bold;
 }
 
-#bookkeeper-overview td.yellow{
-	color:yellow;
-	text-align: right;
+#bookkeeper-overview td.yellow {
+	color: yellow;
+	font-weight: bold;
 }

chrome/overview.js
@@ -168,9 +168,9 @@ var BUILDING_SORT_FUNCTIONS = {
 	ticks: function( a, b ) {
 		return a.ticks - b.ticks;
 	},
-	/*ticksLeft: function( a, b ) {
-		return a.ticks - b.ticks;
-	}*/
+	tleft: function( a, b ) {
+		return a.ticksLeft - b.ticksLeft;
+	}
 };
 
 function compareAsInt( a, b ) {
@@ -242,7 +242,13 @@ function loadBuildings( data ) {
 			b.level = -1;
 		b.stype = BUILDING_SHORTNAMES[ b.type ] || '???';
 		b.ticks = numberOfTicks( b );
-		b.ticksPassed = ticksPassed ( b );
+		if( b.ticks < Infinity ) {
+			b.ticksLeft = b.ticks - ticksPassed(b);
+			if( b.ticksLeft < 0 )
+				b.ticksLeft = 0;
+		}
+		else
+			b.ticksLeft = Infinity;
 		buildings.push( b );
 	}
 
@@ -293,7 +299,7 @@ function showOverviewBuildings( data ) {
 
 	addTH( tr, 'Updated', 'sort', 'bookkeeper-hdr-time' );
 	addTH( tr, 'Ticks', 'sort', 'bookkeeper-hdr-ticks' );
-	addTH( tr, 'Ticks left', 'sort', 'bookkeeper-hdr-ticksLeft' );
+	addTH( tr, 'Left', 'sort', 'bookkeeper-hdr-tleft' );
 
 	addTH( tr, '' ); // the bin icon column
 	thead.appendChild( tr );
@@ -322,9 +328,10 @@ function showOverviewBuildings( data ) {
 
 	function onHeaderClick( event ) {
 		var target = event.target;
+		console.log( 'header click', target );
 		if( target.id && target.id.startsWith( 'bookkeeper-hdr-' ) ) {
 			event.stopPropagation();
-			var newsort = target.id.substr( 12 );
+			var newsort = target.id.substr( 15 );
 			if( newsort == sort )
 				ascending = !ascending;
 			else {
@@ -343,7 +350,8 @@ function showOverviewBuildings( data ) {
 }
 
 function fillTBody( tbody, in_use, buildings, sort, ascending ) {
-	var key, building, tr, cell, img, ckey, n, i , end, j, jend, commodity, sortfn, fn;
+	var key, building, tr, cell, img, ckey, n, i , end, j, jend, commodity,
+	    sortfn, fn, className;
 
 	sortfn = BUILDING_SORT_FUNCTIONS[ sort ];
 	if( sortfn ) {
@@ -392,15 +400,21 @@ function fillTBody( tbody, in_use, buildings, sort, ascending ) {
 
 		addTD( tr, building.ticks < Infinity ? String(building.ticks) : '??', 'r' );
 
-		var tickPassedClass = 'r';
-		if (building.ticks < Infinity) {
+		className = null;
+		if (building.ticksLeft < Infinity) {
 			// Not pretty but fast according to:
 			// https://stackoverflow.com/questions/6665997/switch-statement-for-greater-than-less-than
-			if ((building.ticks - building.ticksPassed ) < 2) { tickPassedClass = "red"; } else
-			if ((building.ticks - building.ticksPassed ) < 3) { tickPassedClass = "yellow"; }
+			if( building.ticksLeft < 1 )
+				className = 'red';
+			else if( building.ticksLeft < 2 )
+				className = 'yellow';
 		}
 
-		addTD( tr, building.ticks < Infinity ? String(building.ticks - building.ticksPassed ) : '??', tickPassedClass );
+		cell = addTD( tr,
+			      building.ticksLeft < Infinity ? String(building.ticksLeft) : '??',
+			      'r' );
+		if( className )
+			cell.classList.add( className );
 
 		img = document.createElement("img");
 		img.src = "http://static.pardus.at/img/stdhq/ui_trash.png";