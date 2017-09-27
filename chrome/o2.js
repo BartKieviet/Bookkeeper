7894cc6393394dd2870b83fd173ddcdc6ea3801e
Val <val@dssrzs.org>
2017-09-27 15:10:06 -0500

    Re-adding sorting and stuff
    Sorting is better now, all modular like the rest of this object now, and
    it displays the current sort criterion.
    
    Fixes #42.

--- chrome/o2.js
+++ chrome/o2.js
@@ -7,68 +7,105 @@ var Overview = (function() {
 
 var ROWSPEC = {
 	location: {
-		header: simpleHeader( 'loc', 'Location' ),
+		header: simpleHeader( 'Location' ),
 		cell: simpleCell( function( b ) {
 			var coords = Sector.getCoords( b.sectorId, b.loc );
 			return Sector.getName( b.sectorId ) +
 				' [' + coords.x + ',' + coords.y + ']';
-		} )
+		} ),
+		sortKey: 'loc',
+		sort: function( a, b ) {
+			var c = compare(
+				Sector.getName(a.sectorId),
+				Sector.getName(b.sectorId) );
+			if ( c === 0 )
+				return a.loc - b.loc;
+			return c;
+		}
 	},
 
+	// This one is useful when the overview is filtered by sector: show only
+	// coords, and don't bother doing the sector name comparison in sort.
 	locationNav: {
-		// This one is useful when we filtered by sector: show only coords.
-		header: simpleHeader( 'loc', 'Loc' ),
+		header: simpleHeader( 'Loc' ),
 		cell: rCell( function( b ) {
 			var coords = Sector.getCoords( b.sectorId, b.loc );
 			return coords.x + ',' + coords.y;
-		} )
+		} ),
+		sortKey: 'loc',
+		sort: function( a, b ) { return a.loc - b.loc; }
 	},
 
 	type: {
-		header: simpleHeader( 'typ', 'Type' ),
-		cell: simpleCell( function( b ) { return b.getTypeShortName() } )
+		header: simpleHeader( 'Type' ),
+		cell: simpleCell(
+			function( b ) { return b.getTypeShortName() } ),
+		sortKey: 'type',
+		sort: function( a, b ) {
+			return compare(
+				a.getTypeShortName(), b.getTypeShortName() );
+		}
 	},
+
 	owner: {
-		header: simpleHeader( 'own', 'Owner' ),
-		cell: simpleCell( function( b ) { return b.owner } )
+		header: simpleHeader( 'Owner' ),
+		cell: simpleCell( function( b ) { return b.owner } ),
+		sortKey: 'owner',
+		sort: function( a, b ) { return compare( a.owner, b.owner ); }
 	},
+
 	level: {
-		header: simpleHeader( 'lvl', 'Lvl' ),
-		cell: rCell( function( b ) { return b.level || '?' } )
+		header: simpleHeader( 'Lvl' ),
+		cell: rCell( function( b ) { return b.level || '?' } ),
+		sortKey: 'level',
+		sort: function( a, b ) { return a.level - b.level; },
+		initDesc: true
 	},
+
 	time: {
-		header: simpleHeader( 'tm', 'Time' ),
-		cell: rCell( function( b ) { return formatTime( b.time * 1000 ); } )
+		header: simpleHeader( 'Time' ),
+		cell: rCell(
+			function( b ) { return formatTime( b.time * 1000 ); } ),
+		sortKey: 'time',
+		sort: function( a, b ) { return a.time - b.time; },
+		initDesc: true
 	},
+
 	ticksLeft: {
-		header: simpleHeader( 'tk', 'Tick' ),
-		cell: rCell( function( b ) { return b.ticksLeft; } )
+		header: simpleHeader( 'Tick' ),
+		cell: rCell( function( b ) { return b.ticksLeft; } ),
+		sortKey: 'tick',
+		sort: function( a, b ) { return a.ticksLeft - b.ticksLeft; }
 	},
+
 	ticksNow: {
-		header: simpleHeader( 'now', 'Now' ),
-		cell: rCell( function( b, now ) { return b.ticksNow( now ); } )
+		header: simpleHeader( 'Now' ),
+		cell: rCell( function( b ) { return b.ticksNow( this.now ); } ),
+		sortKey: 'now',
+		sort: function( a, b ) {
+			return a.ticksNow(this.now) - b.ticksNow(this.now);
+		}
 	}
-}
+};
 
 // The following three funcs are used in the initialisation of the catalogue
-// above.  These are functions that construct functions.  Yes, I know, black
-// magic, I'm sorry :(
+// above.  These are functions that create functions.  Yes, I know, black magic,
+// I'm sorry :(
 
-function simpleHeader( key, legend ) {
+function simpleHeader( legend ) {
 	return function( th ) {
 		th.textContent = legend;
-		th.dataset.col = key;
 	};
 }
 
 function simpleCell( fn ) {
-	return function( now, building, td ) {
+	return function( building, td ) {
 		td.textContent = fn.call( this, building );
 	}
 }
 
 function rCell( fn ) {
-	return function( now, building, td ) {
+	return function( building, td ) {
 		td.textContent = fn.call( this, building );
 		td.className = 'r';
 	}
@@ -90,24 +127,44 @@ function Overview( options ) {
 	}
 }
 
-Overview.prototype.bindToDocument = function( doc, callback ) {
-	var keys;
+Overview.prototype.bindToDocument = function( doc ) {
+	var elements;
 
 	if ( this.doc !== undefined )
 		throw 'Overview instance already bound!';
 
 	this.doc = doc;
 
-	keys = [ this.sortCritKey, this.sortAscKey ];
+	elements = {
+		container: this.doc.createElement('div'),
+		table: this.doc.createElement('table'),
+		head: this.doc.createElement('thead'),
+		rows: this.doc.createElement('tbody'),
+		foot: this.doc.createElement('tfoot')
+	};
 
-	// If we're embedded in nav, then we won't show a location column by
-	// default; rather, we'll fetch the current sector from storage.
-	if ( this.mode === 'nav-embedded' )
-		keys.push( 'sector' );
+	elements.container.id = 'overview';
 
-	chrome.storage.local.get( keys, onStorageData.bind(this) );
+	elements.table.appendChild( elements.head );
+	elements.table.appendChild( elements.rows );
+	elements.table.appendChild( elements.foot );
+	elements.container.appendChild( elements.table );
 
-	function onStorageData( data ) {
+	elements.head.addEventListener(
+		'click', onHeaderClick.bind(this), false );
+
+	this.elements = elements;
+}
+
+Overview.prototype.refresh = function( callback ) {
+	var keys, list;
+
+	// First, get data from local storage.
+
+	keys = [ 'sector', this.sortCritKey, this.sortAscKey ];
+	chrome.storage.local.get( keys, onStorageLocalData.bind(this) );
+
+	function onStorageLocalData( data ) {
 		var sckey = data[ this.sortCritKey ];
 		if ( ROWSPEC[sckey] === null )
 			this.sortCrit = 'time';
@@ -116,25 +173,20 @@ Overview.prototype.bindToDocument = function( doc, callback ) {
 		this.sortAsc = !!data[ this.sortAscKey ];
 		this.sector = data.sector;
 		this.sectorId = Sector.getId( this.sector );
-		makeUI.call( this );
-		callback();
+
+		// Now get the building list from sync
+
+		chrome.storage.sync.get( this.ukey, onHaveListData.bind(this) );
 	}
-}
-
-Overview.prototype.refresh = function( callback ) {
-	var list;
-
-	// First, get fresh data from storage.
-	chrome.storage.sync.get( this.ukey, onHaveListData.bind(this) );
 
 	function onHaveListData( data ) {
-		list = data[ this.ukey ].map( bkey.bind(this) );
-		// Fetch all buildings
-		chrome.storage.sync.get( list, onHaveBuildingData.bind(this) );
-	}
 
-	function bkey( loc ) {
-		return this.ukey + loc;
+		// Now fetch all buildings
+
+		list = data[ this.ukey ].map( bkey.bind(this) );
+		chrome.storage.sync.get( list, onHaveBuildingData.bind(this) );
+
+		function bkey( loc ) { return this.ukey + loc; }
 	}
 
 	function onHaveBuildingData( data ) {
@@ -149,6 +201,7 @@ Overview.prototype.refresh = function( callback ) {
 
 		this.list = list;
 		this.buildings = bldgs;
+		this.now = Building.now();
 
 		clearElement( this.elements.rows );
 		clearElement( this.elements.foot );
@@ -156,32 +209,114 @@ Overview.prototype.refresh = function( callback ) {
 
 		makeRowSpec.call( this );
 		makeHead.call( this );
-		makeRows.call( this );
 
 		if ( callback )
 			callback();
 	}
 }
 
-function makeRowSpec() {
-	var rowSpec;
+Overview.prototype.sort = function( sortKey, asc ) {
+	var sort, fn;
 
+	if ( this.sortKey !== undefined )
+		this.sorts[ this.sortKey ].th.classList.remove( 'asc', 'dsc' );
+
+	sort = this.sorts[ sortKey ];
+
+	if ( asc === undefined ) {
+		if ( this.sortKey === sortKey )
+			this.sortAsc = !this.sortAsc;
+		else
+			this.sortAsc = sort.initAsc;
+	}
+	else
+		this.sortAsc = asc;
+	this.sortKey = sortKey;
+
+	if ( this.sortAsc ) {
+		fn = sort.fn;
+		sort.th.classList.add( 'asc' );
+	}
+	else {
+		fn = function(a, b) { return -sort.fn.call(this, a, b) };
+		sort.th.classList.add( 'dsc' );
+	}
+
+	this.buildings.sort( fn.bind(this) );
+
+	clearElement( this.elements.rows );
+	makeRows.call( this );
+}
+
+function makeRowSpec() {
 	// XXX - we'll change this when filtering
 
-	rowSpec = [];
-	this.rowSpec = rowSpec;
+	this.rowSpec = [];
 	this.commodities = getCommoditiesInUse( this.buildings );
+	this.sorts = {};
 
-	rowSpec.push(
+	this.rowSpec.push(
 		ROWSPEC.locationNav, ROWSPEC.type, ROWSPEC.owner, ROWSPEC.level );
-	this.commodities.forEach( pushComm );
-	rowSpec.push( /*ROWSPEC.time,*/ ROWSPEC.ticksLeft, ROWSPEC.ticksNow );
+	this.commodities.forEach( pushComm.bind(this) );
+	this.rowSpec.push(
+		/*ROWSPEC.time,*/ ROWSPEC.ticksLeft, ROWSPEC.ticksNow );
 
 	function pushComm( commId ) {
-		rowSpec.push( {
+		this.rowSpec.push( {
 			header: makeCommHeaderFn( commId ),
-			cell: makeCommCellFn( commId )
+			cell: makeCommCellFn( commId ),
+			sortKey: commId,
+			sort: makeCommSortFn( commId ),
+			initDesc: true
 		} );
+	}
+}
+
+// Construct the TH elements in the table head.  Assume the row is already empty.
+// Called with `this` set as an Overview instance
+function makeHead() {
+	var tr =  this.doc.createElement( 'tr' );
+	this.rowSpec.forEach( makeTH.bind(this) );
+	this.elements.head.appendChild( tr );
+
+	function makeTH( spec ) {
+		var th, sortKey;
+
+		th = this.doc.createElement( 'th' );
+		spec.header.call( this, th );
+
+		if ( spec.sort ) {
+			th.dataset.sort = spec.sortKey;
+			this.sorts[ spec.sortKey ] = {
+				th: th,
+				fn: spec.sort.bind( this ),
+				initAsc: !spec.initDesc
+			}
+		}
+
+		tr.appendChild( th );
+	}
+}
+
+// Construct the TD elements in the table row.  Assume the row is already empty.
+// Called with `this` set as an Overview instance
+function makeRows() {
+	var now, doc, rows;
+
+	now = Building.now();
+	this.buildings.forEach( addRow.bind(this) );
+
+	function addRow( building ) {
+		var tr =  this.doc.createElement( 'tr' );
+		tr.dataset.loc = building.loc;
+		this.rowSpec.forEach( makeTD.bind(this) );
+		this.elements.rows.appendChild( tr );
+
+		function makeTD( spec ) {
+			var td = this.doc.createElement( 'td' );
+			spec.cell.call( this, building, td );
+			tr.appendChild( td );
+		}
 	}
 }
 
@@ -203,6 +338,7 @@ function makeCommHeaderFn( commId ) {
 		img.title = comm.n;
 		th.className = 'c';
 		th.appendChild( img );
+		th.dataset.sort = commId;
 	}
 }
 
@@ -211,22 +347,11 @@ function makeCommHeaderFn( commId ) {
 // instance.
 
 function makeCommCellFn( commId ) {
-	return function( now, building, td ) {
-		var comm, n;
-
-		comm = Commodities.getCommodity( commId );
-
-		if ( building.isUpkeep( commId ) &&
-		     building.toBuy[commId] !== undefined ) {
-			n = -building.toBuy[ commId ];
-		}
-		else if ( building.forSale[commId] !== undefined ) {
-			n = building.forSale[ commId ];
-		}
-
+	return function( building, td ) {
+		var n = overviewFigure( building, commId );
 		if ( n !== undefined ) {
 			td.textContent = n;
-			td.title = comm.n;
+			td.title = Commodities.getCommodity( commId ).n;
 			td.className = 'c';
 			if ( n > 0 )
 				td.classList.add( 'lime' );
@@ -238,60 +363,50 @@ function makeCommCellFn( commId ) {
 	};
 }
 
-// Called with `this` set as an Overview instance
-function makeUI() {
-	var elements = {
-		container: this.doc.createElement('div'),
-		table: this.doc.createElement('table'),
-		head: this.doc.createElement('thead'),
-		rows: this.doc.createElement('tbody'),
-		foot: this.doc.createElement('tfoot')
-	};
+function makeCommSortFn( commId ) {
+	return function( a, b ) {
+		a = sortval( overviewFigure(a, commId) );
+		b = sortval( overviewFigure(b, commId) );
+		return a - b;
+	}
 
-	elements.container.id = 'overview';
-
-	elements.table.appendChild( elements.head );
-	elements.table.appendChild( elements.rows );
-	elements.table.appendChild( elements.foot );
-	elements.container.appendChild( elements.table );
-
-	this.elements = elements;
-}
-
-// Construct the TH elements in the table head.  Assume the row is already empty.
-// Called with `this` set as an Overview instance
-function makeHead() {
-	var tr =  this.doc.createElement( 'tr' );
-	this.rowSpec.forEach( makeTH.bind(this) );
-	this.elements.head.appendChild( tr );
-
-	function makeTH( spec ) {
-		var th = this.doc.createElement( 'th' );
-		spec.header.call( this, th );
-		tr.appendChild( th );
+	function sortval( n ) {
+		return n === undefined ? -Infinity : n;
 	}
 }
 
-// Construct the TD elements in the table row.  Assume the row is already empty.
-// Called with `this` set as an Overview instance
-function makeRows() {
-	var now, doc, rows;
+// Given a building `b` and a commodity `comm`, return the number to display in
+// the overview table (negative upkeep, positive production, zero, or undefined
+// if the building doesn't trade in that commodity.
+function overviewFigure( building, commId ) {
+	if ( building.isUpkeep( commId ) &&
+	     building.toBuy[commId] !== undefined )
+		return -building.toBuy[ commId ];
 
-	now = Building.now();
-	this.buildings.forEach( addRow.bind(this) );
+	if ( building.forSale[commId] !== undefined )
+		return building.forSale[ commId ];
 
-	function addRow( building ) {
-		var tr =  this.doc.createElement( 'tr' );
-		tr.dataset.loc = building.loc;
-		this.rowSpec.forEach( makeTD.bind(this) );
-		this.elements.rows.appendChild( tr );
+	return undefined;
+}
 
-		function makeTD( spec ) {
-			var td = this.doc.createElement( 'td' );
-			spec.cell.call( this, now, building, td );
-			tr.appendChild( td );
-		}
+function onHeaderClick( event ) {
+	var target, sort;
+
+	// The target may be the TH, or it may be the IMG we tuck inside the
+	// TH.
+
+	target = event.target;
+	while ( (sort = target.dataset.sort) === undefined &&
+		target !== event.currentTarget &&
+		target.parentElement !== null ) {
+		target = target.parentElement;
 	}
+
+	if ( sort === undefined )
+		return;
+
+	event.stopPropagation();
+	this.sort( sort );
 }
 
 function clearElement( element ) {
@@ -363,6 +478,16 @@ function formatTime( timestamp ) {
 	}
 }
 
+// Compare things that can be compared with === and <, like strings
+// (also numbers, but for those a simple `a - b` is easier.
+
+function compare( a, b ) {
+	if ( a === b )
+		return 0;
+	if ( a < b )
+		return -1;
+	return 1;
+}
 
 return Overview;
 
@@ -381,9 +506,12 @@ function onDOM() {
 function onHaveOptions( opts ) {
 	var overview = new Overview( opts );
 
-	overview.bindToDocument( document, function() {
-		document.body.appendChild( overview.elements.container );
-		overview.refresh();
+	overview.bindToDocument( document );
+	document.body.appendChild( overview.elements.container );
+	overview.refresh( onFresh );
+
+	function onFresh() {
+		overview.sort( 'loc' );
 		console.log( 'overview bound', overview );
-	} );
+	}
 }
